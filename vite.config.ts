7747ad8dbe00e7853f3b96135e6db5336f3aac2a
Lucas <lucas-leite-menezes@hotmail.com>
2023-05-08 01:32:27 -0300

    fix: fixing errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(root, "index.html"),
-        about: resolve(root, "about", "index.html"),
-      },
+  server: {
+    watch: {
+      usePolling: true,
     },
   },
 });
